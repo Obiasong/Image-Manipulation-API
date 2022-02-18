3f289e22b75bb9d5800d33ac2251ce679f8f643c
flako <frankarrey@gmail.com>
2022-02-18 05:59:12 +0100

    All initial db tables, Models and respective migrations for easy setup

---
+++ database/migrations/2022_02_18_044401_create_image_manipulations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('image_manipulations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('path', 2000);
+            $table->string('type', 25);
+            $table->text('data');
+            $table->string('output_path', 2000);
+            $table->foreignIdFor(\App\Models\User::class, 'Users_id');
+            $table->foreignIdFor(\App\Models\Album::class, 'Albums_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image_manipulations');
+    }
+};
